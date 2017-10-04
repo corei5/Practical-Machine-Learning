7bb8eb93d884c0890ab0add4a7a87e0f9e9aec6a
corei5 <rabby2186@gmail.com>
2017-10-04 15:24:11 +0800

    final code for Regression Into

--- Regression.py
+++ Regression.py
@@ -1,5 +1,13 @@
 import pandas as pd
-import Quandl
+import quandl
 
-df = Quandl.get('WIKI/GOOGL')
+df = quandl.get('WIKI/GOOGL')
+
+#print(df.head())
+
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close']*100.0
+df['PCT_Change'] = (df['Adj. Close'] - df['Adj. Open'])/ df['Adj. Open']*100.0
+
+df = df[['Adj. Close','HL_PCT','PCT_Change','Adj. Volume']]
 print(df.head())
